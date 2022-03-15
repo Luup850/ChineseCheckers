7d8572062da3ef06ce1b8ab570184015fd724f6d
Marcus <marcusdchr@gmail.com>
2022-03-15 10:11:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from board import GameBoard
 from minimaxAI import AI 
 import numpy as np
 import copy
+import time
 
 #TO DO
 
@@ -10,16 +11,27 @@ import copy
 #UPDATE THE GOALS TO ONE LOCATION BACKWARDS WHEN A GOAL IS REACHED
 
 
-a = GameBoard(6)
-player = AI(1)
+game = GameBoard(6)
+ai = AI(1)
  
 
 def checkmovement(movefrom,moveto):
-    print("MOVEFROM:", movefrom)
+    print("MOVEFROM:", movefrom[1:3])
 
-    AImovefrom = "b0501"
-    AImoveto = "b0511"
-    return AImovefrom, AImoveto
+    game.updateBoard(3,[int(movefrom[3:5]), int(movefrom[1:3])], [int(moveto[3:5]), int(moveto[1:3])], 
+                        game.playerlist[3].index([int(movefrom[3:5]), int(movefrom[1:3])]))
+    
+    print(game._board)
+    move = ai.chooseMove(game, 1, 1)
+    AImoveFrom = 'b{0:02d}{1:02d}'.format(move[1][1], move[1][0])
+    AImoveTo = 'b{0:02d}{1:02d}'.format(move[2][1], move[2][0])
+    print("To from: {0}, {1}".format(AImoveFrom, AImoveTo))
+    #a.updateBoard(1, [10, 2], [12,8], 3)
+    print(move[0], move[1], move[2], move[3])
+    game.updateBoard(move[0], move[1], move[2], move[3])
+    print(game._board)
+
+    return AImoveFrom, AImoveTo
 
 # print(a._board[20,4])
 # print(a._board[18,6])
@@ -36,12 +48,12 @@ def checkmovement(movefrom,moveto):
 # print(a._board)
 
 #print(a._board.shape)
-print(a._board)
-move = player.chooseMove(a, 1, 1)
-#a.updateBoard(1, [10, 2], [12,8], 3)
-print(move[0], move[1], move[2], move[3])
-a.updateBoard(move[0], move[1], move[2], move[3])
-print(a._board)
+# print(a._board)
+# move = player.chooseMove(a, 1, 1)
+# a.updateBoard(1, [10, 2], [12,8], 3)
+# print(move[0], move[1], move[2], move[3])
+# a.updateBoard(move[0], move[1], move[2], move[3])
+# print(a._board)
 #scopy = copy.deepcopy(a._board)
 #print(player.findOptimalMove(a, 1))
 
