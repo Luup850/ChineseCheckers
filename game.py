47a57cb9c8082baea5945162d8010742320ba0e1
mariamsmr <s212488@student.dtu.dk>
2022-03-17 17:35:44 +0100

    popups

--- game.py
+++ game.py
@@ -32,29 +32,42 @@ class CheckersTable(GridLayout):
             self.selectPosition(position)
              
         else:
-            self.moveto = position.name
-            self.move(position)
-            self.selected = None
-            AInumber, AImovefrom, AImoveto = AI.checkmovement(self.movefrom,self.moveto)
-            print("------AI moves------",AImoveto)
-            if AImoveto != "" :
-                colors = [(20/255.0,199/255.0,0/255.0,1), (252/255.0,233/255.0,3/255.0,1), (252/255.0,144/255.0,3/255.0,1), (20/255.0,199/255.0,0/255.0,1), (159/255.0,4/255.0,207/255.0,1), (0,0,1,1)]
-                for i,a in enumerate(AImoveto):
-                    self.ids[a].color_testing = colors[AInumber[i]]
-                for i,a in enumerate(AImovefrom):
-                    self.ids[a].color_testing = (177/255.0,179/255.0,181/255.0,1)
+            if position.color_testing == [0.6941176470588235, 0.7019607843137254, 0.7098039215686275, 1]:
+                self.moveto = position.name
+                self.move(position)
+                self.selected = None
+                AInumber, AImovefrom, AImoveto = AI.checkmovement(self.movefrom,self.moveto)
+                print("------AI moves------",AImoveto)
+                if AImoveto != "" :
+                    colors = [(20/255.0,199/255.0,0/255.0,1), (252/255.0,233/255.0,3/255.0,1), (252/255.0,144/255.0,3/255.0,1), (20/255.0,199/255.0,0/255.0,1), (159/255.0,4/255.0,207/255.0,1), (0,0,1,1)]
+                    for i,a in enumerate(AImoveto):
+                        self.ids[a].color_testing = colors[AInumber[i]]
+                    for i,a in enumerate(AImovefrom):
+                        self.ids[a].color_testing = (177/255.0,179/255.0,181/255.0,1)
 
+            elif position.color_testing != [0.6941176470588235, 0.7019607843137254, 0.7098039215686275, 1]:
+                popup = Popup(title='Warning:',
+                    content=Label(text='Ups! That gap is occupied by another peg, please select an empty space.'),
+                    size_hint=(0.8, 0.2))
+                popup.open()
+
+            elif position.color_testing == [0,0,0,1]:
+                popup = Popup(title='Warning:',
+                    content=Label(text='Ups! You have already selected that peg. Remember that you must select the desire position.'),
+                    size_hint=(0.8, 0.2))
+                popup.open()
+                
             
     def selectPosition(self, position):
-        print(position.color_testing)
+        # print(position.color_testing)
         if position.color_testing != [0.6941176470588235, 0.7019607843137254, 0.7098039215686275, 1]: 
             self.selected = position
             position.color_testing = (0,0,0,1)
 
         elif position.color_testing == [0.6941176470588235, 0.7019607843137254, 0.7098039215686275, 1]: 
-            popup = Popup(
-                    content=Label(text='Please remember to select a coloured piece to be moved'),
-                    size_hint=(None, None),pos_hint={"x":0.5,"top":0.5} ,size=(40, 40))
+            popup = Popup(title='Warning:',
+                    content=Label(text='Ups! That is an empty space, please select a red peg to be moved.'),
+                    size_hint=(0.8, 0.2))
             popup.open()
             
     def move(self,position):
@@ -62,7 +75,6 @@ class CheckersTable(GridLayout):
         print("------Player moves-------",position.name)
         position.color_testing = self.previous_color
         self.selected.color_testing = (177/255.0,179/255.0,181/255.0,1)
-        # self.ids.output_label.text = f'You selected:{position.name}'
         popup = Popup(title='Nice move!!',
                     content=Label(text='Now AI moves'),
                     size_hint=(0.2, 0.2),size=(400, 400))
