e96ac2e4fe651cbe51849f41f2dede5951f0cf93
mariamsmr <s212488@student.dtu.dk>
2022-03-21 17:29:14 +0100

    cleaning

--- game.py
+++ game.py
@@ -8,7 +8,6 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty, ListProperty
 from kivy.lang import Builder
 from kivy.core.window import Window
-from kivy.graphics import RoundedRectangle, Color
 import main as AI
 import time
 
@@ -23,8 +22,6 @@ class CheckersTable(GridLayout):
     selected = ObjectProperty(None, allownone=True)
 
     def press(self,position):
-         
-        # self.id_list = self.ids
         
         if self.selected == None:
             self.movefrom = position.name
@@ -59,7 +56,7 @@ class CheckersTable(GridLayout):
                 
             
     def selectPosition(self, position):
-        # print(position.color_testing)
+        
         if position.color_testing != [0.6941176470588235, 0.7019607843137254, 0.7098039215686275, 1]: 
             self.selected = position
             position.color_testing = (0,0,0,1)
@@ -88,99 +85,6 @@ class gameApp(App):
         
         return ChineseCheckersLayout()
      
-#if __name__ == '__main__':
+## MAIN ##
 app = gameApp()
 app.run()
-
-
-# from kivy.uix.behaviors import ButtonBehavior
-# from kivy.uix.image import Image
-# from kivy.uix.button import Button
-# from kivy.properties import NumericProperty, ReferenceListProperty
-# from kivy.vector import Vector
-
-
-# print(self.selected)
-        #print(position.name)
-#print(self.id_list)
-        #print(self.ids)
-        
-        #self.ids.b0501.text = "X"
-        #self.ids.b0501.color_testing = (177/255.0,179/255.0,181/255.0,1)
- #print(position.text)
-            #print(self.selected.text)
-
- #print(dir(position))
-        #print(position)
-        #position.canvas.clear()
-        
-        #print(position.color_testing)
-
-#position.background_color: rgba(177/255.0,79/255.0,96/255.0,1)
-            #position.text = "X"
-
-#validar el movimiento
-        # print(self)
-        # print(position)
-#self.ids.b0501.text = "X"
-
-
-
-
-
-# table = ObjectProperty(None)
-
-    # print(f'TABLE: {table} ')
-    
-    # def reset(self):
-    #     print(self.ids)
-    #     # for position in self.table.ids:
-        #     if self.table.ids[position] == ""
-        #         self.table.ids[position].text = "O"
-        #self.table.ids.b0012.text = 'tt'
-        #self.table.ids.b0012.background_color = (0,0,1,1)
-        #self.table.ids.b0111.text = 'X'
-        #self.table.ids.b0113.text = 'X'
-    #reset(table)
-
-# class BoubleButton(ButtonBehavior, Image):
-#     def __init__(self, **kwargs):
-#         super(BoubleButton, self).__init__(**kwargs)
-#         self.source = directory+'bouble.png'
-
-    # def on_press(self):
-    #     self.source = 'atlas://data/images/defaulttheme/checkbox_on'
-
-    # def on_release(self):
-    #     self.source = 'atlas://data/images/defaulttheme/checkbox_off'
-# class BoubleButton(Button):
-#     pass
-    # Print to the screen
-        #     # self.add_widget(Label(text=f'Welcome you are going to be {players} players'))
-            #print(f'Position selected: {self.selected}')
-            
-        #     # Clear the input boxes
-        #     self.players.text = ""
-
-# def reset(self):
-    #     print(self.id_list)
-    #     self.id_list.b0502.text = "X"
-
-    #     # self.ids.color_testing = (177/255.0,179/255.0,181/255.0,1)
-    #     #Green
-    #     green = (20/255.0,199/255.0,0/255.0,1)
-    #     self.id_list.b0012.color_testing = green
-    #     self.ids.b0111.color_testing = green
-    #     self.ids.b0113.color_testing = green
-    #     self.ids.b0210.color_testing = green
-    #     self.ids.b0212.color_testing = green
-    #     self.ids.b0214.color_testing = green
-    #     self.ids.b0309.color_testing = green
-    #     self.ids.b0311.color_testing = green
-    #     self.ids.b0313.color_testing = green
-    #     self.ids.b0315.color_testing = green
-    #     #Yellow
-    #     #Orange
-    #     #Red
-        #Purple
-        #Blue
